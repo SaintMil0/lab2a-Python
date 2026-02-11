42cc068b21e79d331261850a35ebd47a27d06a50
SaintMil0 <97435029+SaintMil0@users.noreply.github.com>
2026-02-11 01:39:23 -0800

    lab.py

---
+++ lab.py
@@ -0,0 +1,10 @@
+print("All the world's a stage,")
+print("And all the men and women merely players:")
+print("They have their exits and their entrances;")
+print("And one man in his time plays many parts,")
+print("His acts being seven ages.")
+
+
+paragraph = "All the world's a stage, And all the men and women merely players: They have their exits and their entrances; And one man in his time plays many parts, His acts being seven ages."
+print(paragraph)
+
